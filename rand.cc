90a66155744e8c767f753e6a94b288c15bcfd895
Yan Liu <yan.liu@owl.phy.queensu.ca>
2019-08-13 18:03:06 -0400

    A simple code that loops and spits out clock ticks.

--- rand.cc
+++ rand.cc
@@ -9,14 +9,21 @@ int main() {
   chain.Add("./ntuple_files/Analysis*.ntuple.root");
 
   Int_t nhits;
+  ULong64_t clockCount50, tick=0, tickdiff;
   chain.SetBranchAddress("nhits", &nhits);
+  chain.SetBranchAddress("clockCount50", &clockCount50);
   Int_t total = chain.GetEntries();
 
   for(int i=0; i<total; i++)
     {
-      cout << i << endl;
       chain.GetEvent(i);
-      cout << nhits << endl;
+      tickdiff = clockCount50-tick;
+      tick = clockCount50;
+      cout << tick << " -  " <<clockCount50 <<"   =   "<< tickdiff<< endl;
+
+      //DO NOT REMOVE
+      //monitor clock jump
+      if (tickdiff>9999999999999) return 2;
     }
 
   return 0;
