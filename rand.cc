771a6f9176a588ac4941d3bd94ea34232d12a0cf
Yan Liu <yan.liu@owl.phy.queensu.ca>
2019-08-13 18:48:47 -0400

    Add triggerWord, datacleaning and FitValid.

--- rand.cc
+++ rand.cc
@@ -12,19 +12,31 @@ int main() {
 
   TH1D *hTime = new TH1D("hTime","Time difference", 2000,0.,2000);
 
-  Int_t nhits;
+  bool fitValid;
+  int nhits, triggerWord;
   ULong64_t clockCount50, curtick, tickdiff;
+  ULong64_t dcFlagged, dcApplied, dcClean = 0xFB0000017FFE;
   chain.SetBranchAddress("nhits", &nhits);
   chain.SetBranchAddress("clockCount50", &clockCount50);
+  chain.SetBranchAddress("fitValid", &fitValid);
+  chain.SetBranchAddress("dcFlagged", &dcFlagged);
+  chain.SetBranchAddress("dcApplied", &dcApplied);
+  chain.SetBranchAddress("triggerWord", &triggerWord);
 
   //first event
   chain.GetEvent(0);
   curtick = clockCount50;
 
-  //for(int iEv=1; iEv<chain.GetEntries(); iEv++)
-  for(int iEv=1; iEv<9990000; iEv++)
+  for(int iEv=1; iEv<chain.GetEntries(); iEv++)
+  //for(int iEv=1; iEv<9990000; iEv++)
     {
       chain.GetEvent(iEv);
+
+      //triggerWord, data-cleaning, fitValid
+      if (!(((triggerWord & 0x401400) == 0x0) && (triggerWord != 0x40))) continue;
+      if (((dcApplied & dcClean) & dcFlagged) != (dcApplied & dcClean)) continue;
+      if (!fitValid) continue;
+
       tickdiff = clockCount50-curtick;
       curtick = clockCount50;
 
