17743834dab90c6c28eb16886cf329c4fb342625
Yan Liu <yan.liu@owl.phy.queensu.ca>
2019-08-15 19:00:29 -0400

    Now add another prompt branch to scandata.

--- src/rand.cc
+++ src/rand.cc
@@ -10,33 +10,29 @@ using namespace std;
 
 void rand_assign(string scanfile, string treename, const int step, const vector<double> &prompt_cuts) {
 
-  cout << "HERE" << endl;
   TFile file(scanfile.c_str(), "UPDATE"); 
-  cout << "HERE" << endl;
   TTree *scan = (TTree *)file.Get(treename.c_str());
-  cout << "HERE" << endl;
 
   float prompt;
   //int nhits;
-  //double energy;
+  double energy;
   //scan->SetBranchAddress("nhits", &nhits);
-  //scan->SetBranchAddress("energy", &energy);
+  scan->SetBranchAddress("energy", &energy);
 
   TBranch *promptBr = scan->Branch("prompt", &prompt, "prompt/F");
 
   cout << scan->GetEntries() << endl;
   for(int iEv=0; iEv<scan->GetEntries(); iEv++) {
-      //scan->GetEvent(iEv);
+      scan->GetEvent(iEv);
 
       //applying cuts
       prompt = 1.;
-      //if (energy>prompt_cuts[0]) prompt = true;
+      if (energy>prompt_cuts[0]) prompt = true;
       promptBr->Fill();
     }
 
-  cout << "HERE" << endl;
   scan->Write("", TObject::kOverwrite);
-  //file.Close();
+  file.Close();
 
   return;
 }
