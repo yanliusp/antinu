8206ee61e85910bc29549532f0b18fc71eb70b0c
Yan Liu <yan.liu@owl.phy.queensu.ca>
2019-08-16 11:19:37 -0400

    clean up printouts.

--- src/coincidence.cc
+++ src/coincidence.cc
@@ -43,9 +43,6 @@ void coincidence(string scanfile, string treename, const vector<double> &coincid
   for(int iEv=0; iEv<scan->GetEntries(); iEv++) {
     scan->GetEvent(iEv);
 
-    //initialize
-    tickdiff50_4 = 0; posdiff_4 = 0; //DO I NEED THIS?
-
     //applying cuts
     if(prompt) {
       //extract prompt time & position info
@@ -53,15 +50,12 @@ void coincidence(string scanfile, string treename, const vector<double> &coincid
       promptposv = {posx,posy,posz};
       promptrunID = runID;
       prompteventID = eventID;
-      cout << "promptID to be acted on: " << promptrunID <<" " <<prompteventID << "  " <<iEv << endl;
 
       tag = false;
       for(int next=1;!tag;next++) {
         scan->GetEvent(iEv+next);
-        cout << "searching for delayed event: " << iEv+next  << " " << delayed<< endl;
-        if(iEv+next >=scan->GetEntries()) tag=true; //end loop
+        if(iEv+next >= scan->GetEntries()) tag=true; //end loop
         if(delayed) {
-          //cout << next << endl;
           tag=true;
           //calculate time difference
           tickdiff50_4 = clockCount50 - promptick50;
@@ -70,12 +64,9 @@ void coincidence(string scanfile, string treename, const vector<double> &coincid
           posv = {posx,posy,posz};
           for (int i=0;i<3;i++) posdiff_4 = posdiff_4 + pow (posv[i]-promptposv[i], 2.0);
           posdiff_4 = sqrt(posdiff_4);
-          if(tickdiff50_4*20./1000.<coincidence_cuts[0]) cout << "yes" << endl;
-          if(posdiff_4<coincidence_cuts[1]) {
-            //cout << "pos: " << posdiff_4 << "   time: " << tickdiff50_4*20./1000. << endl;
-            cout << "promptID: " << promptrunID <<" " <<prompteventID <<
-                  "  delayedID: " << runID <<" " << eventID << endl; 
-          }
+          if(tickdiff50_4*20./1000.<coincidence_cuts[0]) cout <<"yes" << endl; //PLACEHOLDER
+          if(posdiff_4<coincidence_cuts[1]) cout << "yes\r" << flush; //PLACEHOLDER
+
           candidate.Fill();
         }
       }
