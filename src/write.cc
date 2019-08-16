4ef23757666adde492b79669c5b78df6d1cc373c
Yan Liu <yan.liu@owl.phy.queensu.ca>
2019-08-16 18:56:43 -0400

    change to use pointer.

--- src/write.cc
+++ src/write.cc
@@ -13,21 +13,21 @@ using namespace std;
 void write(const string &branchname, const vector<double> & coincidence_cuts) {
 
   TFile *writeFile = new TFile("./count.root","RECREATE");//CHANGE PATH LATER
-  TTree count("count", "final count");
+  TTree *nodata = new TTree("nodata", "number of events that passes cuts");
 
   int step, event;
 
-  count.Branch(branchname.c_str(), &event, (branchname+"/I").c_str());
-  count.Branch("step", &step, "step/I");
+  nodata->Branch(branchname.c_str(), &event, (branchname+"/I").c_str());
+  nodata->Branch("step", &step, "step/I");
 
 
   for(step=0; step<1000; step++) {
     event = randomize("./candidate_files/candidate.root", "candidate", step, coincidence_cuts);
-    count.Fill();
+    nodata->Fill();
   }
   
   writeFile->cd();
-  count.Write("final");
+  nodata->Write();
   writeFile->Close();
   cout << "write to " << "./count.root" << endl;
 
