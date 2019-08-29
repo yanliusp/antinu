c6d6f39832af32ae64b3b9d8045434ee16f5f1e5
Yan Liu <yan.liu@owl.phy.queensu.ca>
2019-08-29 15:48:38 -0400

    edit main to test coincidence().

--- src/main.cc
+++ src/main.cc
@@ -22,36 +22,36 @@ inline bool exists (const string& name) {
 
 int main () {
 
-  const string SKIMDIR = "./skim_files/skim_s0.root";
-  const string SCANDIR = "./scan_files/scan.root";
-  //const string ntuplepath = "/data/snoplus/processed_data/timebin_5-2/*ntuple.root";
-  const string ntuplepath = "./ntuple_files/*ntuple.root";
-  // number of entries per skim file
-  int filesize = 1000000;
-
-  // cuts
-  const vector<double> global_cuts = {5300., 0.0, 0.55, -0.12, 0.95}; //FV, u.r, itr, beta14_min, beta14_max
-  const vector<double> prompt_cuts={2.5,0.9,1000.}; //energy, nhits_min, nhits_max
-  const vector<double> delayed_cuts={1.5,9.9,20.1}; //energy, nhits_min, nhits_max
-  const vector<double> coincidence_cuts={15/20.*1000,2000.}; //time [us], position
-
-  //convert cuts to a string
-  ostringstream ocuts;
-  copy(global_cuts.begin(), global_cuts.end(), std::ostream_iterator<double>(ocuts, "_"));
-  copy(prompt_cuts.begin(), prompt_cuts.end(), std::ostream_iterator<double>(ocuts, "_"));
-  copy(delayed_cuts.begin(), delayed_cuts.end(), std::ostream_iterator<double>(ocuts, "_"));
-  copy(coincidence_cuts.begin(), coincidence_cuts.end(), std::ostream_iterator<double>(ocuts, "_"));
-  const string cuts = ocuts.str();
-
-  //make skim files from ntuples(data)
-  if (!exists(SKIMDIR)) skim(ntuplepath, filesize);
-
-  //apply global cuts
-  if (!exists(SCANDIR)) scan(global_cuts);
+//  const string SKIMDIR = "./skim_files/skim_s0.root";
+//  const string SCANDIR = "./scan_files/scan.root";
+//  //const string ntuplepath = "/data/snoplus/processed_data/timebin_5-2/*ntuple.root";
+//  const string ntuplepath = "./ntuple_files/*ntuple.root";
+//  // number of entries per skim file
+//  int filesize = 1000000;
+//
+//  // cuts
+//  const vector<double> global_cuts = {5300., 0.0, 0.55, -0.12, 0.95}; //FV, u.r, itr, beta14_min, beta14_max
+  const vector<double> prompt_cuts={0.0,0.9,1000.}; //energy, nhits_min, nhits_max
+  const vector<double> delayed_cuts={0.0,0.9,1000.}; //energy, nhits_min, nhits_max
+//  const vector<double> coincidence_cuts={15/20.*1000,2000.}; //time [us], position
+//
+//  //convert cuts to a string
+//  ostringstream ocuts;
+//  copy(global_cuts.begin(), global_cuts.end(), std::ostream_iterator<double>(ocuts, "_"));
+//  copy(prompt_cuts.begin(), prompt_cuts.end(), std::ostream_iterator<double>(ocuts, "_"));
+//  copy(delayed_cuts.begin(), delayed_cuts.end(), std::ostream_iterator<double>(ocuts, "_"));
+//  copy(coincidence_cuts.begin(), coincidence_cuts.end(), std::ostream_iterator<double>(ocuts, "_"));
+//  const string cuts = ocuts.str();
+//
+//  //make skim files from ntuples(data)
+//  if (!exists(SKIMDIR)) skim(ntuplepath, filesize);
+//
+//  //apply global cuts
+//  if (!exists(SCANDIR)) scan(global_cuts);
 
   //apply individual cuts
-  event_select("./scan_files/scan.root","scandata", "prompt", prompt_cuts);
-  event_select("./scan_files/scan.root","scandata", "delayed", delayed_cuts);
+  event_select("./your_ntuple.root","output", "prompt", prompt_cuts);
+  event_select("./your_ntuple.root","output", "delayed", delayed_cuts);
 
   //apply coincidence cuts
   coincidence("./scan_files/scan.root","scandata");
@@ -60,7 +60,7 @@ int main () {
   //Method: "Moving to the next"
   //randomize("./candidate_files/candidate.root", "candidate", 0, coincidence_cuts);
 
-  write(cuts, coincidence_cuts);
+//  write(cuts, coincidence_cuts);
 
   return 0;
 }
